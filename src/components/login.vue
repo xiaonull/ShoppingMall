6a95ca24de6d62671227a6cde0f22940dc95dd18
xiaonull <xiaonull@foxmail.com>
2017-09-30 17:11:48 +0800

    V1.0.0
    搭建环境

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+	
+</template>
+
+<script>
+	export default {
+		name: 'login',
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/login.vue'
+import Home from '@/components/home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+	routes: [{
+		path: '/',
+		name: 'login',
+		component: Login
+	},{
+		path: '/home',
+		name: 'home',
+		component: Home
+	}]
+})
