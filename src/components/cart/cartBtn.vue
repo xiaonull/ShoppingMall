e681c120ec24e748ac2c679ca85f8e2bbd905c60
黄庭森 <xiaonull@foxmail.com>
2017-10-06 00:15:58 +0800

    V1.3.1

---
+++ src/components/cart/cartBtn.vue
@@ -0,0 +1,36 @@
+<template>
+	<section class="cartBtn">
+		<span @click="change"><i class="fa" :class="{'fa-circle-o': checked_ === false, 'check-circle': checked_}"></i></span>
+	</section>
+</template>
+
+<script>
+	export default {
+		props: ['checked'],
+		data() {
+			return {
+				checked_ : false
+			}
+		},
+		create() {
+			this.checked_ = this.checked;
+		},
+		methods: {
+			change() {
+				this.checked_ = !this.checked_;
+				this.$emit('change');
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	.cartBtn {
+		display: inline-block;
+
+		.fa {
+			font-size: 10px;
+			color: #26a2ff;
+		}
+	}
+</style>
---
+++ src/components/cart/countBtnGroup.vue
@@ -0,0 +1,49 @@
+<template>
+	<section class="countBtnGroup">
+		<span class="count-btn">-</span>
+		<span class="count-btn counter">{{counter}}</span>
+		<span class="count-btn">+</span>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				counter: 1
+			}
+		},
+		methods: {
+			plus() {
+				this.counter++;
+				this.$emit('plus');
+			},
+			minus() {
+				if(this.counter > 0) {
+					this.counter--;
+					this.$emit('minus');
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	.countBtnGroup {
+		display: inline-block;
+
+		.count-btn {
+			display: inline-block;
+			width: 20px;
+			height: 20px;
+			text-align: center;
+			line-height: 20px;
+			border: 1px solid #D9D5D5;
+			color: #26a2ff;
+		}
+
+		.counter {
+			color: #000;
+		}
+	}
+</style>
